282b2f463a786f7d6fa20d1254b0da45537375d7
CptBreeza <cbasicok@gmail.com>
2019-01-08 16:40:51 +0800

    Create Resizer

--- lib/scixir.ex
+++ lib/scixir.ex
@@ -1,18 +1,2 @@
 defmodule Scixir do
-  @moduledoc """
-  Documentation for Scixir.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Scixir.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/scixir_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ScixirTest do
-  use ExUnit.Case
-  doctest Scixir
-
-  test "greets the world" do
-    assert Scixir.hello() == :world
-  end
-end

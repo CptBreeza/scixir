42611dd727f39ff930157e131239c7a7b3d89e0a
CptBreeza <cbasicok@gmail.com>
2019-01-08 16:10:19 +0800

    Introduce Mogrify

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule Scixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:mogrify, "~> 0.7.0"}
     ]
   end
 end

a8d48f6b94094a9e5912b2e609f5630bfd68f68c
CptBreeza <cbasicok@gmail.com>
2019-03-01 15:14:43 +0800

    Normalize configs

--- config/config.exs
+++ config/config.exs
@@ -1,18 +1,6 @@
 use Mix.Config
 
-config :scixir, :redis,
-  url: "redis://localhost:6379",
-  notification_key: "minio_events",
-  worker: 5
-
 config :logger, :console,
   format: "$dateT$time $metadata[$level] $levelpad$message\n"
 
-config :ex_aws,
-  access_key_id: "JV2ECX06XID7AG9FRDO1",
-  secret_access_key: "0ny0EwDb39S9PJdaRhGuwKdW1H7TXFYapn8JJM71"
-
-config :ex_aws, :s3,
-  scheme: "http://",
-  host: "127.0.0.1",
-  port: "9000"
+import_config "#{Mix.env}.exs"

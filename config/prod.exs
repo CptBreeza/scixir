81d4a6a2d915d2656b214080876204ff8846f521
fahchen <06fahchen@gmail.com>
2019-07-03 21:30:44 +0800

    Log process.log

--- config/prod.exs
+++ config/prod.exs
@@ -13,3 +13,20 @@ config :ex_aws, :s3,
   scheme: "http://",
   host: System.get_env("MINIO_HOST"),
   port: System.get_env("MINIO_PORT")
+
+logger_level =
+  case System.get_env("MIX_LOGGER_LEVEL") do
+    nil -> :info
+    "" -> :info
+    level when is_binary(level) -> String.to_atom(level)
+  end
+
+# Do not print debug messages in production
+config :logger, level: logger_level
+config :logger,
+  backends: [{LoggerFileBackend, :file}, :console]
+
+config :logger, :file,
+  path: "/var/log/scixir/process.log",
+  format: "$time $metadata[$level] $message\n",
+  level: logger_level
